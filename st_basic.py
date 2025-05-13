99cb429d95c778a9d9478816c0c355d216a91e37
youngju <164602664+Dokki27@users.noreply.github.com>
2025-05-13 17:29:00 +0900

    Add files via upload

---
+++ st_basic.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('첫번째 웹 어플 만들기🔥🫶')
+st.write('#1. Markdown 텍스트 작성하기')
+st.markdown(
+    '''
+    # 마크다운 헤더1
+    - 마크다운 목록1. **굵게** 표시
+    - 마크다운 목록2.  *기울임* 표시
+    '''
+)
